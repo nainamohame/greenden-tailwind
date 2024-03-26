8cc39521fbced8c32a65e710ceb129e38af4dd57
nainamohame <nainamohmed111@gmail.com>
2024-03-26 23:42:29 +0530

    Added Js

---
+++ contact.js
@@ -0,0 +1,16 @@
+var sidenav=document.getElementById("sidenav")
+
+var menuicon=document.getElementById("menuicon")
+
+var closenav=document.getElementById("close-nav")
+
+menuicon.addEventListener("click",function(event){
+    sidenav.style.right=0
+
+})
+
+
+
+closenav.addEventListener("click",function(){
+    sidenav.style.right="-50%"
+})
